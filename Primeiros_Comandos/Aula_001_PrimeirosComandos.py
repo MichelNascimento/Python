5b19f062eb31829ecfd274c37004e063fe1f2664
Michel Nascimento <michel.lopes.n@gmail.com>
2023-07-12 15:41:55 -0300

    cor

--- Primeiros_Comandos/Aula_001_PrimeirosComandos.py
+++ Primeiros_Comandos/Aula_001_PrimeirosComandos.py
@@ -9,19 +9,18 @@ print('Olá', 5)      #### PRINT PARA CONTATENAR STRING + NUMERO
 
 #---------------------------------------------------------#
 
-#TODA VARIAVEL EM PYTHON SÀO OBJETOS
+# TODA VARIAVEL EM PYTHON SÀO OBJETOS
 nome = 'Michel'
 idade = 34
 peso  = 67.8
 
 #print(nome + idade + peso) NÃO É POSSIVEL CONCATENAR TIPO DE VÁRIAVEIS DIFERENTES UTILIZANDO O +.
-print(nome, idade, peso) #FORMA CORREAT PARA CONTATENAR TIPOS DIFERENTES UTILIZANDO A VIRGULA ,.
-
+print(nome, idade, peso) #FORMA CORRETA PARA CONCATENAR TIPOS DIFERENTES UTILIZANDO A VIRGULA ,.
 #--------------------------------------------------------------------------------------#
 # INTERATIVIDADE COM ENTRADA DE DADOS
 
-nome = input('Qual é o seu nome: ')
-idade = input('Qual a sua idade: ')
-peso = input('Qual o seu peso: ')
-print(nome, idade, peso)
+#nome = input('Qual é o seu nome: ')
+#idade = input('Qual a sua idade: ')
+#peso = input('Qual o seu peso: ')
+#print(nome, idade, peso)
 # FOI CRIADO UM SCRIPT PARA SER EXECUTADOS aula01
